a2180f9483accd37400c9c35aeb2c0f1f41e6853
Guillermo Cabral <guille.cabral87@gmail.com>
2021-08-19 17:27:10 -0400

    leave some message

--- app.js
+++ app.js
@@ -1,24 +1,70 @@
 const express = require('express');
+const morgan = require('morgan');
+
 //express app
 const app = express();
 
 //listen for requests
 app.listen(3000);
 
-app.get('/', (req, res) =>{
-    res.sendFile('./views/index.html', {root: __dirname});
+//middleware & static files
+app.use(morgan('dev'));
+
+// app.use((req, res, next) => {
+//     console.log('new request made:');
+//     console.log('host: ', req.hostname);
+//     console.log('path: ', req.path);
+//     console.log('method: ', req.method);
+//     next();
+//   });
+
+//   app.use((req, res, next) => {
+//     console.log('in the next middleware');
+//      next();
+//    });
+
+//register view engine
+app.set('view engine', 'ejs');
+
+const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+
+app.get('/', (req, res) => {
+    res.render('index', {title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', { title: 'About' });
 });
 
-//redirects
-app.get('/about-us', (req, res) =>{
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog' });
 });
+
+app.use(express.static('public'));
 
 //404 page
 app.use((req, res) =>{
-  res.status(404).sendFile('./views/404.html', {root: __dirname});
-});
+    res.render('404', { title: '404' });
+  });
+
+// // 404 page
+// app.use((req, res) => {
+//     res.status(404).render('404');
+// });
+
+// app.get('/', (req, res) =>{
+//     res.sendFile('./views/index.html', {root: __dirname});
+// });
+
+// app.get('/about', (req, res) => {
+//     res.sendFile('./views/about.html', {root: __dirname});
+// });
+
+// //redirects
+// app.get('/about-us', (req, res) =>{
+//     res.redirect('/about');
+// });
